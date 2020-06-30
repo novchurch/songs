9717e0c0bf5caa9158b1b10d20d0651fc0f360d6
Philippe Rigovanov <phil@rigovanov.ru>
2020-06-30 21:38:08 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,25 @@
 ---
 ---
-const staticCacheName = 'static-cache-novchurch-songs-v000';
-const dynamicCacheName = 'dynamic-cache-novchurch-songs-v000';
+const staticCacheName = 'static-cache-novchurch-songs-v001';
+const dynamicCacheName = 'dynamic-cache-novchurch-songs-v001';
 
 const staticAssets = [
-    '.{{ "/" | relative_url }}',
-    '.{{ "/alphabet/" | relative_url }}',
+    '{{ "/" | relative_url }}',
+    '{{ "/alphabet/" | relative_url }}',
     {% for post in site.posts %}'.{{ post.url | relative_url }}',{% endfor %}
-    '.{{ "/images/icons/icon-180x180.png" | relative_url }}',
-    '.{{ "/images/icons/icon-512x512.png" | relative_url }}',
-    '.{{ "/offline/" | relative_url }}',
-    {% if site.minimal %}'.{{ "/assets/css/minimal.css" | relative_url }}',
-    '.{{ "/assets/css/classes.css" | relative_url }}',
-    {% else %}'.{{ "/assets/css/style.css" | relative_url }}',{% endif %}{% if site.sidebar %}'.{{ "/assets/css/sidebar.css" | relative_url }}',{% endif %}
-    '.{{ "/assets/fonts/PTSans-Regular.woff" | relative_url }}',
-    '.{{ "/assets/fonts/PTSans-Bold.woff" | relative_url }}',
-    '.{{ "/images/icons/hymnbook.svg" | relative_url }}',
-    '.{{ "/images/icons/mask-hymnbook.svg" | relative_url }}',
-    '.{{ "/assets/fontawesome/icons.svg" | relative_url }}',
-    '.{{ "/js/app.js" | relative_url }}',
-    '.{{ "/images/no-image.jpg" | relative_url }}'
+    '{{ "/images/icons/icon-180x180.png" | relative_url }}',
+    '{{ "/images/icons/icon-512x512.png" | relative_url }}',
+    '{{ "/offline/" | relative_url }}',
+    {% if site.minimal %}'{{ "/assets/css/minimal.css" | relative_url }}',
+    '{{ "/assets/css/classes.css" | relative_url }}',
+    {% else %}'{{ "/assets/css/style.css" | relative_url }}',{% endif %}{% if site.sidebar %}'{{ "/assets/css/sidebar.css" | relative_url }}',{% endif %}
+    '{{ "/assets/fonts/PTSans-Regular.woff" | relative_url }}',
+    '{{ "/assets/fonts/PTSans-Bold.woff" | relative_url }}',
+    '{{ "/images/icons/hymnbook.svg" | relative_url }}',
+    '{{ "/images/icons/mask-hymnbook.svg" | relative_url }}',
+    '{{ "/assets/fontawesome/icons.svg" | relative_url }}',
+    '{{ "/js/app.js" | relative_url }}',
+    '{{ "/images/no-image.jpg" | relative_url }}'
 ];
 
 self.addEventListener('install', async event => {
@@ -61,9 +61,9 @@ async function checkOnline(req) {
         if (cachedRes) {
             return cachedRes;
         } else if (req.url.indexOf('.jpg') !== -1 || req.url.indexOf('.jpeg') !== -1 || req.url.indexOf('.png') !== -1  || req.url.indexOf('.svg') !== -1) {
-            return caches.match('.{{ "/images/no-image.jpg" | relative_url }}');
+            return caches.match('{{ "/images/no-image.jpg" | relative_url }}');
         } else {
-            return caches.match('.{{ "/offline/index.html" | relative_url }}');
+            return caches.match('{{ "/offline/index.html" | relative_url }}');
         }
     }
 }

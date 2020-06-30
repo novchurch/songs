ef5d4967fd79a99ed241e224ff492484012aa040
Philippe Rigovanov <phil@rigovanov.ru>
2020-06-30 09:09:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ---
 ---
-const staticCacheName = 'static-cache-v007';
-const dynamicCacheName = 'dynamic-cache-v007';
+const staticCacheName = 'static-cache-v008';
+const dynamicCacheName = 'dynamic-cache-v008';
 
 const staticAssets = [
     '{{ "/" | relative_url }}',

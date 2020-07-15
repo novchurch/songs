35d8170e7497d83f5e0bb10d5cf33234c4d86ef5
Philippe Rigovanov <phil@rigovanov.ru>
2020-07-15 22:55:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,10 +7,7 @@ const staticAssets = [
     '{{ "/" | relative_url }}',
     '{{ "/alphabet/" | relative_url }}',
     {% for post in site.posts %}'{{ post.url | relative_url }}',
-    {% endfor %}'{{ "/images/icons/icon-144x144.png" | relative_url }}',
-    '{{ "/images/icons/icon-180x180.png" | relative_url }}',
-    '{{ "/images/icons/icon-512x512.png" | relative_url }}',
-    '{{ "/offline/" | relative_url }}',
+    {% endfor %}'{{ "/offline/" | relative_url }}',
     {% if site.minimal %}'{{ "/assets/css/minimal.css" | relative_url }}',
     '{{ "/assets/css/classes.css" | relative_url }}',
     {% else %}'{{ "/assets/css/style.css" | relative_url }}',{% endif %}{% if site.sidebar %}'{{ "/assets/css/sidebar.css" | relative_url }}',{% endif %}

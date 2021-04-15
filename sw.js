b7daf6443b4f2058e61c163f870ac822fb707b1e
Philippe Rigovanov <phil@rigovanov.ru>
2021-04-15 11:28:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 ---
 ---
-const staticCacheName = 'static-cache-novchurch-songs-v003';
-const dynamicCacheName = 'dynamic-cache-novchurch-songs-v003';
+const staticCacheName = 'static-cache-novchurch-songs-v004';
+const dynamicCacheName = 'dynamic-cache-novchurch-songs-v004';
 
 const staticAssets = [
     '{{ "/" | relative_url }}',
